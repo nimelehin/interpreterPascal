8fde1e61c10606cdbc6eca857edbab9a7b062516
Nikita <nikascom@Nikitas-iMac.local>
2019-09-14 18:25:19 +0300

    tabs changed

--- Interpreter/__init__.py
+++ Interpreter/__init__.py
@@ -3,9 +3,9 @@ from Interpreter.Parser.parse import Parser
 from Interpreter.types import Type
 from Interpreter.token import Token
 
-def run():
-        inter = Interpret(Parser())
-        while True:
-                str = input()
-                inter.parser.set_code_line(str)
-                print(inter.interpret())
+def run(filename):
+    inter = Interpret(Parser())
+    while True:
+        str = input()
+        inter.parser.set_code_line(str)
+        print(inter.interpret())
--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 from Interpreter import *
 
-run()
+run('a.pas')
